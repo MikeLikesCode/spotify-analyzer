04f43161743da3e9e10f0e90e5606bd2450c244d
Michael Guerrero <maikurusama@gmail.com>
2020-12-15 10:42:59 -0500

    Adding

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
   
-module.exports = {
-    images: {
-      domains: ['i.scdn.co', 'mosaic.scdn.co', 'newjams-images.scdn.co', 'lineup-images.scdn.co', 'seeded-session-images.scdn.co', 'www.searchpng.com'],
-    },
-  }
+const withProgressBar = require('next-progressbar')
+module.exports = withProgressBar({
+  images: {
+    domains: ['i.scdn.co', 'mosaic.scdn.co', 'newjams-images.scdn.co', 'lineup-images.scdn.co', 'seeded-session-images.scdn.co', 'www.searchpng.com'],
+  },
+})
