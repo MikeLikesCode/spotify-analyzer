3091a92c9ec15c718fbb490d67f7e71424efc705
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2022-02-17 18:37:22 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const withProgressBar = require('next-progressbar')
 module.exports = withProgressBar({
   images: {
+    loader: 'imgix',
+    path: '',
     domains: ['i.scdn.co', 'mosaic.scdn.co', 'newjams-images.scdn.co', 'lineup-images.scdn.co', 'seeded-session-images.scdn.co', 'www.searchpng.com'],
   },
 })
